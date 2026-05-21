db6ef11d71c937e7c0928fa6841cebf211ea7d26
Urjam Fals <jumarfal45@gmail.com>
2026-05-21 19:52:16 +0800

    Updated server.ts

--- server.ts
+++ server.ts
@@ -2,19 +2,36 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+
 import errorHandler from './_middleware/error-handler';
 import accountsController from './accounts/accounts.controller';
 import swaggerDocs from './_helpers/swagger';
 
 const app = express();
 
+const corsOrigin =
+    process.env.CORS_ORIGIN;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 // allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+app.use(cors({
+    origin:
+        process.env.NODE_ENV === 'production'
+            ? (
+                corsOrigin
+                    ? corsOrigin
+                        .split(',')
+                        .map(x => x.trim())
+                    : false
+            )
+            : (origin, callback) =>
+                callback(null, true),
 
+    credentials: true
+}));
 // api routes
 app.use('/accounts', accountsController);
 
